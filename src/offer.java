1eb63fbd3219d1606cec29ad8cbeb5fc0eb4772a
ThinerZQ <601097836@qq.com>
2016-03-21 20:41:27 +0800

    Initial commit

---
+++ src/offer.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA
+ * Date: 2016/3/13
+ * Time: 19:31
+ * User: ThinerZQ
+ * GitHub: <a>https://github.com/ThinerZQ</a>
+ * Blog: <a>http://www.thinerzq.me</a>
+ * Email: 601097836@qq.com
+ */
+public class offer {
+    public static void main(String[] args) {
+
+    }
+    public boolean Find(int [][] array,int target) {
+
+        return false;
+    }
+}
